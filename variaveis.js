0b4e53e2af6a132edc77d48606876930c63bd067
matheus fabri <154552723+mhfabri@users.noreply.github.com>
2026-02-23 08:45:27 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,30 @@
+var nomeDaVariavel = "valor da variável";
+var outraVariavel = 'valor da outra variável';
+var variavelIdade = 22;
+var variavelTenhoCabeloBranco = false;
+
+
+
+console.log(nomeDaVariavel);
+console.log(outraVariavel);
+console.log(variavelIdade);
+console.log(variavelTenhoCabeloBranco);
+
+
+
+
+var a = 14;
+var b = 42;
+
+var tr = (a == 14);
+var fl = (a == b);
+
+var int1 = tr+1;
+var int2 = fl+1;
+
+console.log(int1);
+console.log(int2);
+console.log(tr);
+console.log(fl);
+console.log(a);
+console.log(b);
